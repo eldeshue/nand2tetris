7219aae6fc6e7a2898461e501fe0dcf7b669a4e0
iphuck22 <eldeshue@naver.com>
2023-02-13 13:08:57 +0900

    Feat : modify main.cpp.

--- projects/06/Assembler/main.cpp
+++ projects/06/Assembler/main.cpp
@@ -6,15 +6,15 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <queue>
+#include <deque>
 
 #include "Hack_Assembly_Parser.h"
 #include "Hack_Code_module.h"
 #include "Symbol_table.h"
 
 // shared data structure, queue
-std::queue<std::string> input_buffer; 
-std::queue<std::string> output_buffer;
+std::string input_buffer; 
+std::deque<std::string> output_buffer;
 
 int main(int argc, char* argv[]) {
 
@@ -37,31 +37,40 @@ int main(int argc, char* argv[]) {
   std::ifstream input_file(file_name);
   
   // change the name of the file
-  std::string::size_type after_dot = file_name.find('.asm') + 1; // find the locaton
+  std::string::size_type after_dot = file_name.find(".asm") + 1; // find the locaton
   file_name.erase(after_dot);           // erase .asm extension
   file_name.insert(after_dot, "hack");  // add .hack extension
 
   // create and open a .hack file
-  std::ofstream output_file(file_name);
+  std::ofstream output_file(file_name, std::ios::trunc);
 
   // set objects
-  Symbol_table symbol_table();
-  Hack_Assembly_Parser parser();
-  Hack_Code_module();
+  Hack_Assembly_Parser parser;
+  Symbol_table symbol_table;
+  Hack_Code_module encoder;
 
+  // Hack_Assembly_Parser, read a line from file and parse
+  // erase all the blank and comments
+  parser.parse_assembly(input_file, input_buffer);
+  
   // Symbol_table
   // scan all the file and make a table of symbol and it's address
+  symbol_table.scan_assembly(input_buffer);
 
-
-  // thread1: Hack_Assembly_Parser, read a line from file and parse
-  // erase all the blank and comments
-  
-
-  // thread2: Hack_Code_module, translate a word to binary code
+  // thread1: Hack_Code_module, translate a word to binary code
   // + change symbol(string) to address(number)
+  encoder.mnemonic_to_code(input_buffer, output_buffer, symbol_table);
 
+  // thread2: lambda function, write bit strings in the .hack file
+  if(!output_file.is_open()){
+    fprintf(stderr, "output file has a problem");
+    exit(-1);
+  }
 
-  // thread3: lambda function, write bit strings in the .hack file
+  while(output_buffer.size() != 0) {
+    output_file << output_buffer.front() << "\n";
+    output_buffer.pop_front(); 
+  }
 
-
+  return 0;
 }
