f4310134c950c77a446fa11bdee305d9464ce51f
iphuck22 <eldeshue@naver.com>
2023-02-23 15:11:42 +0900

    Feat : implement Parser module for virtual machine.

--- projects/07/VMtranslator/VM_Parser.cpp
+++ projects/07/VMtranslator/VM_Parser.cpp
@@ -8,22 +8,26 @@
 #include "arg1_Type.h"
 #include "arg2_Type.h"
 
-VM_Parser::VM_Parser(std::ifstream& i_stream){
+VM_Parser::VM_Parser(std::ifstream &i_stream)
+{
 
-  if(i_stream.is_open()){
+  if (i_stream.is_open()) // read a file at a time.
+  {
     i_stream.seekg(0, std::ios::end);
     int string_size = i_stream.tellg();
-    input_string_stream.resize(string_size); 
+    input_string_stream.resize(string_size);
 
     i_stream.seekg(0, std::ios::beg);
     i_stream.read(&input_string_stream[0], string_size);
   }
-  else{
+  else // reading error
+  {
     fprintf(stderr, "The vm file is not available.");
     exit(-1);
   }
 
   // initiate hash table
+  // type of command
   arg1_table.insert(std::make_pair("add", C_ADD));
   arg1_table.insert(std::make_pair("sub", C_SUB));
   arg1_table.insert(std::make_pair("neg", C_NEG));
@@ -41,7 +45,8 @@ VM_Parser::VM_Parser(std::ifstream& i_stream){
   arg1_table.insert(std::make_pair("function", C_FUNCTION));
   arg1_table.insert(std::make_pair("return", C_RETURN));
   arg1_table.insert(std::make_pair("call", C_CALL));
-  
+
+  // type of segment
   arg2_table.insert(std::make_pair("argument", ARGUMENT));
   arg2_table.insert(std::make_pair("local", LOCAL));
   arg2_table.insert(std::make_pair("static", STATIC));
@@ -50,9 +55,9 @@ VM_Parser::VM_Parser(std::ifstream& i_stream){
   arg2_table.insert(std::make_pair("that", THAT));
   arg2_table.insert(std::make_pair("pointer", POINTER));
   arg2_table.insert(std::make_pair("temp", TEMP));
-
 }
-VM_Parser::~VM_Parser(){
+VM_Parser::~VM_Parser()
+{
   // clear string
   input_string_stream.clear();
   // clear hash table
@@ -60,26 +65,33 @@ VM_Parser::~VM_Parser(){
   arg2_table.clear();
 }
 ///////////////////////////////////////////////////
-int VM_Parser::arg1Type(std::string& command) {
+int VM_Parser::arg1Type(std::string &command)
+{
   // hash table, unordered map
-  if(arg1_table.find(command) == arg1_table.end()){
+  if (arg1_table.find(command) == arg1_table.end()) // not included
+  {
     return 0;
   }
-  else{
+  else
+  {
     return arg1_table[command];
   }
 }
-int VM_Parser::arg2Type(std::string& command) {
+int VM_Parser::arg2Type(std::string &command)
+{
   // hash table, unordered map
-  if(arg2_table.find(command) == arg2_table.end()){
+  if (arg2_table.find(command) == arg2_table.end())
+  {
     return 0;
   }
-  else{
+  else
+  {
     return arg2_table[command];
   }
 }
 ////////////////////////////////////////////////////
-void eraseComment(std::string& command) {
+void eraseComment(std::string &command)
+{
   std::string::size_type cur_pos = 0;
   std::string::size_type end_pos;
 
@@ -89,9 +101,10 @@ void eraseComment(std::string& command) {
     command.erase(command.begin() + cur_pos, command.begin() + end_pos);
   }
 }
-void eraseEmptyLine(std::string& command) {
+void eraseEmptyLine(std::string &command)
+{
   // erase carriage return
-  command.erase( std::remove(command.begin(), command.end(), '\r'), command.end());
+  command.erase(std::remove(command.begin(), command.end(), '\r'), command.end());
   // erase EOL
   std::string::size_type cur_pos = 0;
   while ((cur_pos = command.find("\n\n", cur_pos)) != std::string::npos)
@@ -108,7 +121,8 @@ void eraseEmptyLine(std::string& command) {
   command.erase(command.end() - 1);
 }
 
-void VM_Parser::parseCommand(std::deque<std::tuple<int, int, int>> buffer){
+void VM_Parser::parseCommand(std::deque<std::tuple<int, int, int>> buffer)
+{
 
   eraseComment(input_string_stream);
   eraseEmptyLine(input_string_stream);
@@ -116,7 +130,8 @@ void VM_Parser::parseCommand(std::deque<std::tuple<int, int, int>> buffer){
   std::istringstream isstream(input_string_stream);
   std::string current_command;
 
-  while(isstream){
+  while (isstream)
+  {
     getline(isstream, current_command);
     std::istringstream cstream(current_command);
 
@@ -126,6 +141,6 @@ void VM_Parser::parseCommand(std::deque<std::tuple<int, int, int>> buffer){
     cstream >> com >> seg >> value;
     buffer.push_back(std::make_tuple(arg1Type(com),
                                      arg2Type(seg),
-                                     value)); 
+                                     value));
   }
 }
--- projects/07/VMtranslator/VM_Parser.h
+++ projects/07/VMtranslator/VM_Parser.h
@@ -3,31 +3,31 @@
 // parse command and classify.
 
 #ifndef _VM_PARSER_
-#define  _VM_PARSER_
+#define _VM_PARSER_
 
 #include <string>
 #include <fstream>
 #include <deque>
 #include <unordered_map>
 
-class VM_Parser {
-  private:
+class VM_Parser
+{
+private:
   std::string input_string_stream;
   std::unordered_map<std::string, int> arg1_table;
   std::unordered_map<std::string, int> arg2_table;
 
-  public:
-  VM_Parser(std::ifstream& i_stream);
+public:
+  VM_Parser(std::ifstream &i_stream);
   ~VM_Parser();
 
-  int arg1Type(std::string& command);  
-  int arg2Type(std::string& command);
+  int arg1Type(std::string &command); // return type of the first arg, operator
+  int arg2Type(std::string &command); // return type of the second arg, segment
 
   void parseCommand(std::deque<std::tuple<int, int, int>> buffer); // put parsed commands to the buffer
-
 };
 
-void eraseComment(std::string& command);
-void eraseEmptyLine(std::string& command);
+void eraseComment(std::string &command);   // erase comments
+void eraseEmptyLine(std::string &command); // erase empty line, \n or \r
 
 #endif
